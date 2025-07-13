b2b7150d63b553f3a5180ae4e823c0a03253feea
rajesh_token <ext.rtripathy2@cma-cgm.com>
2025-07-14 01:19:08 +0530

    chore: sync modules/iam-service-account at tag daily-2021.11.09

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2022 Google LLC
+ * Copyright 2021 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,42 +36,36 @@ variable "iam" {
   description = "IAM bindings on the service account in {ROLE => [MEMBERS]} format."
   type        = map(list(string))
   default     = {}
-  nullable    = false
 }
 
 variable "iam_billing_roles" {
-  description = "Billing account roles granted to the service account, by billing account id. Non-authoritative."
+  description = "Project roles granted to the service account, by billing account id."
   type        = map(list(string))
   default     = {}
-  nullable    = false
 }
 
 variable "iam_folder_roles" {
-  description = "Folder roles granted to the service account, by folder id. Non-authoritative."
+  description = "Project roles granted to the service account, by folder id."
   type        = map(list(string))
   default     = {}
-  nullable    = false
 }
 
 variable "iam_organization_roles" {
-  description = "Organization roles granted to the service account, by organization id. Non-authoritative."
+  description = "Project roles granted to the service account, by organization id."
   type        = map(list(string))
   default     = {}
-  nullable    = false
 }
 
 variable "iam_project_roles" {
   description = "Project roles granted to the service account, by project id."
   type        = map(list(string))
   default     = {}
-  nullable    = false
 }
 
 variable "iam_storage_roles" {
   description = "Storage roles granted to the service account, by bucket name."
   type        = map(list(string))
   default     = {}
-  nullable    = false
 }
 
 variable "name" {
@@ -88,12 +82,6 @@ variable "prefix" {
 variable "project_id" {
   description = "Project id where service account will be created."
   type        = string
-}
-
-variable "public_keys_directory" {
-  description = "Path to public keys data files to upload to the service account (should have `.pem` extension)."
-  type        = string
-  default     = ""
 }
 
 variable "service_account_create" {
--- versions.tf
+++ versions.tf
@@ -1,4 +1,4 @@
-# Copyright 2022 Google LLC
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 terraform {
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.0.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
