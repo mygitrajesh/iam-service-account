60a06281ba7e0e9952ab1a0e147685f2c3a248ae
rajesh_token <ext.rtripathy2@cma-cgm.com>
2025-07-14 01:37:17 +0530

    chore: sync modules/iam-service-account at tag v9.0.1

--- outputs.tf
+++ outputs.tf
@@ -14,23 +14,34 @@
  * limitations under the License.
  */
 
-output "service_account" {
-  description = "Service account resource."
-  value       = local.service_account
-}
-
 output "email" {
   description = "Service account email."
-  value       = local.service_account.email
+  value       = local.resource_email_static
+  depends_on = [
+    local.service_account
+  ]
 }
 
 output "iam_email" {
   description = "IAM-format service account email."
-  value       = local.resource_iam_email
+  value       = local.resource_iam_email_static
+  depends_on = [
+    local.service_account
+  ]
 }
 
 output "key" {
   description = "Service account key."
   sensitive   = true
   value       = local.key
+}
+
+output "service_account" {
+  description = "Service account resource."
+  value       = local.service_account
+}
+
+output "service_account_credentials" {
+  description = "Service account json credential templates for uploaded public keys data."
+  value       = local.service_account_credential_templates
 }
--- variables.tf
+++ variables.tf
@@ -14,6 +14,18 @@
  * limitations under the License.
  */
 
+variable "description" {
+  description = "Optional description."
+  type        = string
+  default     = null
+}
+
+variable "display_name" {
+  description = "Display name of the service account to create."
+  type        = string
+  default     = "Terraform-managed."
+}
+
 variable "generate_key" {
   description = "Generate a key for service account."
   type        = bool
@@ -61,12 +73,6 @@ variable "name" {
   type        = string
 }
 
-variable "display_name" {
-  description = "Display name of the service account to create."
-  type        = string
-  default     = "Terraform-managed."
-}
-
 variable "prefix" {
   description = "Prefix applied to service account names."
   type        = string
@@ -76,6 +82,12 @@ variable "prefix" {
 variable "project_id" {
   description = "Project id where service account will be created."
   type        = string
+}
+
+variable "public_keys_directory" {
+  description = "Path to public keys data files to upload to the service account (should have `.pem` extension)."
+  type        = string
+  default     = ""
 }
 
 variable "service_account_create" {
